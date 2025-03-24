ee21dcec579cff66976d796f83468476ca1d6d06
nikgav1 <nikgav66@gmail.com>
2025-03-24 18:29:33 +0300

    Solved exercises four and five

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function () {
+    const args = Array.from(arguments)
+    let numberSet = args[0];
+    let numberSetRemove = args.slice(1);
+
+    return numberSet.filter(item => !numberSetRemove.includes(item));
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let sum = num1 + num2;
+    if (num1 < 0 || num2 < 0 || !Number.isInteger(num1) || !Number.isInteger(num2)){
+        return 'ERROR';
+    }
 
+    if(num1 > num2){
+        for(let i = num1 - 1; i > num2; i--){
+            sum = sum + i;
+        }
+    }
+    else{
+        for(let i = num1 + 1; i < num2; i++){
+            sum = sum + i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
